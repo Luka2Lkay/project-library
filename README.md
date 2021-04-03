56f5cc7440219c1a3d2be4c50ab6256311b0a76e
Luka <lukhanyo95@gmail.com>
2021-04-03 22:14:04 +0200

    add local storage

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 The Odin Project: Project-Library
 
 This is a small basic library application programmed in HTML, CSS and Javascript.
+
+live preview: https://luka2lkay.github.io/project-library/
--- script.js
+++ script.js
@@ -1,171 +1,151 @@
 
 const subBtn = document.getElementById('submit');
 const myForm = document.getElementById('my_form');
-const addBtn = document.getElementById('add_btn')
-const cancelBtn = document.getElementById('cancel')
-const libraryContainer = document.getElementById('show_library')
+const addBtn = document.getElementById('add_btn');
+const cancelBtn = document.getElementById('cancel');
+const libraryContainer = document.getElementById('show_library');
+const bookTitle = document.getElementById('title');
+const bookAuthor = document.getElementById('author');
+const bookPages = document.getElementById('pages');
+const bookRead = document.getElementById('read');
 
-
-/*initialise library*/
-
+//initialise library
 let myLibrary = [];
 
-/*Book constructor*/
+//Book constructor
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id = Date.now();
 }
 
-
-
-/*shows the form when you click on the add button and clear the input fields when you click Cancel*/
+//shows the form when you click on the add button and clear the input fields when you click Cancel
 function showHideForm() {
     if (addBtn) {
         myForm.style.display = 'block';
-
-    }
+     };
 
     if (addBtn) {
         addBtn.style.display = 'none';
-
-    }
-}
+    };
+};
 
 cancelBtn.addEventListener('click', (e) => {
     addBtn.style.display = 'block';
     myForm.style.display = 'none';
     myForm.reset();
+});
 
-
-})
-
-/* Hide the form when your click submit*/
+//Hide the form when your click submit
 myForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    /* when the submit button is clicked, addBtn value changes from Cancel to Add Book*/
+    //when the submit button is clicked, addBtn value changes from Cancel to Add Book
     if (subBtn) {
 
         addBtn.style.display = 'block';
-        /*if (addBtn.value === 'Add Book') {
-            addBtn.value = 'Cancel'
-            myForm.reset();
-        } else {
-            addBtn.value = 'Add Book';
-        }*/
-
-        /* The form becomes invisible*/
+        
+        //The form becomes invisible
         myForm.style.display = 'none';
+    };
 
-    }
+    //argument for addBookToLibrary
+    title = bookTitle.value;
+    author = bookAuthor.value;
+    pages = bookPages.value;
+    read = bookRead.checked;
+    id = this.id;
 
+    let newBook = new Book(title, author, pages, read);
 
-    /* argument for addBookToLibrary*/
-
-    myFormValue = e.target.elements;
-    let newBook = new Book(myFormValue.title.value, myFormValue.author.value, myFormValue.pages.value, myFormValue.read.checked);
-
-    /* push newBook into myLibrary*/
-    myLibrary.push(newBook)
-
-    /*render new book on submission*/
-    addBookToLibrary(newBook)
-
+    //push newBook into myLibrary
+    myLibrary.push(newBook);
+   
+    //render new book on submission
+    addToLocalStorage(myLibrary);
+    updateLibrary();
     myForm.reset();
+});
 
-})
+const updateLibrary = () =>{
+    libraryContainer.querySelectorAll('div').forEach(n => n.remove());
 
-const addBookToLibrary = (myBook) => {
+    for(let i=0; i< myLibrary.length; i++){
+        renderBook(myLibrary[i])
+    };
+};
 
-    let formDiv = document.createElement('form')
-    formDiv.classList.add('toggle_remove');
+// create a book card with the book info
+const renderBook = (book) =>{
+    const bookCard = document.createElement('div');
+    bookCard.setAttribute('id', book.id);
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+    const bookTitlePara = document.createElement('p');
+    const bookAuthorPara = document.createElement('p');
+    const bookPagesPara = document.createElement('p');
+    const bookReadPara = document.createElement('p');
+   
+    //classes
+    bookCard.classList.add('book_card');
+    readBtn.classList.add('read_button');
+    removeBtn.classList.add('remove_button');
+    bookTitlePara.classList.add('book_title');
+    bookAuthorPara.classList.add('book_author');
+    bookPagesPara.classList.add('book_pages');
+    bookReadPara.classList.add('read');
 
-    /* buttons to delete book button*/
-    let removeBtn = document.createElement('input');
-    removeBtn.setAttribute('type', 'button');
-    removeBtn.classList.add('delete');
-    removeBtn.value = 'Remove'
+    bookTitlePara.innerHTML = `<strong>Title</straong>: ${book.title}`;
+    bookAuthorPara.innerHTML = `<strong>Author</strong>: ${book.author}`;
+    bookPagesPara.innerHTML = `<strong>Pages: ${book.pages}`;
 
+    removeBtn.innerText = 'REMOVE';
 
-    /* Read toggle button*/
-    let readBtn = document.createElement('input');
-    readBtn.setAttribute('type', 'button');
-    readBtn.classList.add('readBtn_style');
-
-    /* append input to form*/
-
-    formDiv.appendChild(removeBtn);
-    formDiv.appendChild(readBtn);
-
-
-    /* seperate book entries*/
-
-    const containerDiv = document.createElement('div');
-    containerDiv.classList.add('bookContainer');
-
-    /* append containerDiv to libraryContainer*/
-
-    libraryContainer.appendChild(containerDiv);
-
-    /*display book details*/
-
-    for (const [key, value] of Object.entries(myBook)) {
-        /* create a P element for every detail*/
-
-        const pNode = document.createElement('p');
-        /*logic for displaying book information on screen*/
-
-        if (key === 'pages') {
-            pNode.innerHTML = `<strong>Pages</strong>: ${value}`;
-            pNode.classList.add('num_pages');
-        }
-
-        else if (key === 'read') {
-            if (value) {
-                readBtn.value = 'Read: Yes';
-                readBtn.style.background = 'lightgreen'
-            } else {
-                readBtn.value = 'Read: No';
-                readBtn.style.background = 'orange'
-            }
-        }
-
-        else if (key === 'author') {
-            pNode.innerHTML = `<strong>Author</strong>: ${value}`;
-            pNode.classList.add('book_author');
-        } else {
-            pNode.innerHTML = `<strong>Title</strong>: ${value}`;
-            pNode.classList.add('book_title');
-        }
-
-        pNode.classList.add("bookDetail")
-        containerDiv.append(pNode)
-        containerDiv.append(readBtn)
+    if(book.read === false){
+        readBtn.innerText = 'NOT READ';
+        readBtn.style.backgroundColor = 'white';
+    }else{
+        readBtn.innerText = 'READ';
+        readBtn.style.backgroundColor = 'lightGreen';
     }
-
-    containerDiv.appendChild(removeBtn);
-
-    /*remove book*/
-
+    
+    readBtn.addEventListener('click', () =>{
+       book.read = !book.read;
+        addToLocalStorage(myLibrary)
+        updateLibrary();
+    })
+    
     removeBtn.addEventListener('click', () => {
-        containerDiv.remove();
+        myLibrary.splice(myLibrary.indexOf(book), 1)
+        addToLocalStorage(myLibrary);
+        updateLibrary();
     })
 
-    /*toggle read button*/
-
-    readBtn.addEventListener('click', () => {
-        if (readBtn.value === 'Read: No') {
-            readBtn.style.background = 'lightgreen';
-            readBtn.value = 'Read: Yes';
-        } else if (readBtn.value === 'Read: Yes') {
-            readBtn.value = 'Read: No';
-            readBtn.style.background = 'orange'
-        }
-
-    })
-
+    bookCard.appendChild(bookTitlePara);
+    bookCard.appendChild(bookAuthorPara);
+    bookCard.appendChild(bookPagesPara);
+    bookCard.appendChild(readBtn);
+    bookCard.appendChild(removeBtn);
+    libraryContainer.appendChild(bookCard);
 }
 
 
+// Save to the local storage
+const addToLocalStorage = (myLibrary) => {
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary))
+    updateLibrary();
+};
+
+const getFromLocalStorage = () =>{
+    myLibrary = JSON.parse(localStorage.getItem('myLibrary'));
+
+    if(myLibrary === null){
+        myLibrary = [];
+    }else{
+        updateLibrary();
+    }
+}
+
+getFromLocalStorage();
